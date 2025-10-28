0a2d6f7f0356f0d7956442f2c9cb379c21040284
xpr <rs.wug@proton.me>
2025-10-28 16:13:41 -0400

    Create extensions.h

---
+++ include/extensions.h
@@ -0,0 +1,45 @@
+#pragma once
+
+/*
+ * Syntax highlighting keywords and extensions shared across modules.
+ */
+
+extern char *C_HL_extensions[];
+extern char *C_HL_keywords[];
+
+extern char *Py_HL_keywords[];
+extern char *Py_HL_extensions[];
+
+extern char *Rust_HL_keywords[];
+extern char *Rust_HL_extensions[];
+
+extern char *Lua_HL_keywords[];
+extern char *Lua_HL_extensions[];
+
+extern char *TS_HL_keywords[];
+extern char *TS_HL_extensions[];
+extern char *TSX_HL_extensions[];
+
+extern char *Zig_HL_keywords[];
+extern char *Zig_HL_extensions[];
+
+extern char *TOML_HL_keywords[];
+extern char *TOML_HL_extensions[];
+
+extern char *Nix_HL_keywords[];
+extern char *Nix_HL_extensions[];
+
+extern char *CMake_HL_keywords[];
+extern char *CMake_HL_extensions[];
+
+extern char *RiscV_HL_keywords[];
+extern char *RiscV_HL_extensions[];
+
+extern char *CS_HL_keywords[];
+extern char *CS_HL_extensions[];
+
+extern char *Make_HL_keywords[];
+extern char *Make_HL_extensions[];
+
+extern char *V_HL_keywords[];
+extern char *V_HL_extensions[];
